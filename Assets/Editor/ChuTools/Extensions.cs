9573f1f832eab45070cb985b9c07dba32bb5e9cb
Jeremy Barzas <jbarzas@gmail.com>
2018-05-30 19:10:45 -0500

    work on node editor

--- Assets/Editor/ChuTools/Extensions.cs
+++ Assets/Editor/ChuTools/Extensions.cs
@@ -1,34 +1,37 @@
 using UnityEngine;
 
-public static class Extensions
+namespace ChuTools
 {
-    public static void MoveDown(this Rect rect, int amount)
+    public static class Extensions
     {
-        rect.position = new Vector2(rect.x, rect.y + amount);
-    }
+        public static void MoveDown(this Rect rect, int amount)
+        {
+            rect.position = new Vector2(rect.x, rect.y + amount);
+        }
 
-    public static void SetX(this Vector3 v3, int value)
-    {
-        v3 = new Vector3(value, v3.y, v3.z);
-    }
+        public static void SetX(this Vector3 v3, int value)
+        {
+            v3 = new Vector3(value, v3.y, v3.z);
+        }
 
-    public static void SetY(this Vector3 v3, int value)
-    {
-        v3 = new Vector3(v3.x, value, v3.z);
-    }
+        public static void SetY(this Vector3 v3, int value)
+        {
+            v3 = new Vector3(v3.x, value, v3.z);
+        }
 
-    public static void SetZ(this Vector3 v3, int value)
-    {
-        v3 = new Vector3(v3.x, v3.y, value);
-    }
+        public static void SetZ(this Vector3 v3, int value)
+        {
+            v3 = new Vector3(v3.x, v3.y, value);
+        }
 
-    public static void SetX(this Vector2 v2, int value)
-    {
-        v2 = new Vector3(value, v2.y);
-    }
+        public static void SetX(this Vector2 v2, int value)
+        {
+            v2 = new Vector3(value, v2.y);
+        }
 
-    public static void SetY(this Vector2 v2, int value)
-    {
-        v2 = new Vector3(v2.x, value);
+        public static void SetY(this Vector2 v2, int value)
+        {
+            v2 = new Vector3(v2.x, value);
+        }
     }
 }
---
+++ Assets/Editor/JeremyTools/EditorBaseWindow.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class JButton
+{
+    public Rect Rect;
+    public int ID;
+    public JButton(Vector3 position, Vector2 size)
+    {
+        Rect = new Rect(position, size);
+        ID = 0;
+    }
+
+    public void Draw()
+    {
+        GUI.Box(Rect, GUIContent.none);
+        GUILayout.BeginArea(Rect);//group an area so it stays when this button moves
+        GUILayout.Label(ID.ToString());
+        GUILayout.EndArea();
+    }
+    public void UpdateData()
+    {
+
+    }
+
+    public void PollEvents(Event e)
+    {
+        switch (e.type)
+        {
+            case EventType.MouseDown:
+                if (Event.current.button == 0)
+                {
+                    ID = GetHashCode();
+                    GUI.changed = true;
+                    
+                }
+
+                if (Event.current.button == 1)
+                {
+                    if (Rect.Contains(Event.current.mousePosition))
+                    {
+                        var gm = new GenericMenu();
+                        gm.AddItem(new GUIContent("click me"), false, () => { Debug.Log("clicked"); });
+                        gm.ShowAsContext();
+                        Event.current.Use();
+                    }
+
+                }
+                break;
+        }
+    }
+}
+public class EditorBaseWindow : EditorWindow
+{
+    // fields
+    private Rect background;
+    public List<JButton> buttons = new List<JButton>();
+    bool isDrag = false;
+    Rect startRect, endRect;
+
+    // methods
+    [MenuItem(itemName: "JeremyTools/NodeWindow")]
+    static void OpenWindow()
+    {
+        var w = CreateInstance<EditorBaseWindow>();
+        w.Show();
+
+    }
+    void OnEnable()
+    {
+        buttons = new List<JButton>()
+        {
+            new JButton(new Vector3(Screen.width / 2, Screen.height / 2, 0), new Vector2(150, 50))
+        };
+    }
+
+    void PollEvents(Event e)
+    {
+        switch (e.type)
+        {
+            case EventType.MouseDown:
+                mdcount++;
+                if (Event.current.button == 0)
+                {
+                    isDrag = true;
+                    startRect = new Rect(Event.current.mousePosition, Vector2.one);
+                    endRect = startRect;
+                    Event.current.Use();
+                    
+                }
+                if (Event.current.button == 1)
+                {
+                    cccount++;
+                    var gm = new GenericMenu();
+                    gm.AddItem(new GUIContent("add button"), false, () => { CreateButton(e.mousePosition); });
+                    gm.ShowAsContext();
+                }
+                break;
+        }
+
+       
+    }
+   
+    public int mdcount, mucount, cccount, mdrcount;
+    void OnGUI()
+    {
+        PollEvents(Event.current);
+        UpdateData();
+        Draw();
+        buttons.ForEach(b => b.PollEvents(Event.current));
+        buttons.ForEach(b => b.UpdateData());
+        buttons.ForEach(b => b.Draw());
+        Repaint();
+
+    }
+
+    void CreateContextMenu(Event e)
+    {
+
+    }
+
+    void CreateButton(Vector2 pos)
+    {
+        buttons.Add(new JButton(pos, new Vector2(150, 50)));
+    }
+
+    void UpdateData()
+    {
+        background.size = new Vector2(40, 40);
+        background.position = new Vector3(1, 1, 1);
+    }
+
+    void Draw()
+    {
+
+        if (GUILayout.Button("DoIt"))
+            ClearConsole();
+        EditorGUILayout.IntField("button count", buttons.Count);
+        EditorGUILayout.LabelField("start timer", Time.realtimeSinceStartup.ToString());
+
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.RectField("startRect", startRect);
+        EditorGUILayout.RectField("endRect", endRect);
+        GUILayout.EndHorizontal();
+        GUILayout.Space(5);
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("mdcount: ", mdcount.ToString());
+        EditorGUILayout.LabelField("mucount: ", mucount.ToString());
+        EditorGUILayout.LabelField("cccount: ", cccount.ToString());
+        EditorGUILayout.LabelField("mdrcount: ", mdrcount.ToString());
+        GUILayout.EndHorizontal();
+        GUI.Box(background, new GUIContent("new Box 1"), GUIStyle.none);
+    }
+
+    static void ClearConsole()
+    {
+        var logEntries = System.Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+    }
+}
