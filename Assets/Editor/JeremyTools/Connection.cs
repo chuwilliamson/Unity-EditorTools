2b1f704caae1bfb218bfd1a5e35e88f35132ee53
Jeremy Barzas <jbarzas@gmail.com>
2018-06-01 12:44:54 -0500

    broke classes into seperate files

---
+++ Assets/Editor/JeremyTools/Connection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace JeremyTools
+{
+    public class Connection
+    {
+        INode inNode;
+        INode outNode;
+
+        public Connection(INode inN, INode outN)
+        {
+            inNode = inN;
+            outNode = outN;
+        }
+
+        public void Draw()
+        {
+            Handles.DrawLine(inNode.InCenter, outNode.OutCenter);
+        }
+    }
+}
