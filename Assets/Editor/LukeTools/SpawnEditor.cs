ce8212c3dcccae660733d815e57c3bc8eecb6f5e
lukeplaisance <luke.plaisance17@gmail.com>
2018-06-05 11:36:52 -0500

    now able to drag button and spawn object

--- Assets/Editor/LukeTools/SpawnEditor.cs
+++ Assets/Editor/LukeTools/SpawnEditor.cs
@@ -16,22 +16,61 @@ namespace LukeTools
             var w = ScriptableObject.CreateInstance<SpawnEditor>();
             w.Show();
         }
-
+        Rect buttonRect = new Rect(x: 50, y: 50, width: 150, height: 100);
         private Object Prefab;
+        private GameObject SpawnedPrefab;
+
         void Draw()
         {
-            var buttonRect = new Rect(x: 50, y: 50, width: 150, height: 150);
             var content = new Rect(buttonRect);
-            GUI.Box(buttonRect, "object");
+            GUI.Box(buttonRect, "");
             GUILayout.BeginArea(buttonRect);
+            GUILayout.Label(buttonRect.position.ToString());
             Prefab = EditorGUILayout.ObjectField(Prefab, typeof(GameObject), false);
             GUILayout.EndArea();
         }
 
 
+
+        public bool isSelected = false;
+        public void PollEvents()
+        {
+            switch (Event.current.type)
+            {
+                case EventType.MouseDown:
+                    if (Event.current.button == 0)
+                    {
+                        DestroyImmediate(SpawnedPrefab);
+                        SpawnedPrefab = Instantiate(Prefab, buttonRect.position, Quaternion.identity) as GameObject;
+                        if (buttonRect.Contains(Event.current.mousePosition))
+                        {
+                            isSelected = true;
+                            GUI.changed = true;
+                        }
+                        else
+                        {
+                            isSelected = false;
+                            GUI.changed = true;
+                        }
+                    }
+                    break;
+                case EventType.MouseDrag:
+                    if (Event.current.button == 0)
+                    {
+                        if (isSelected)
+                        {
+                            buttonRect.position += Event.current.delta;
+                            Event.current.Use();
+                        }
+                    }
+                    break;
+            }
+        }
+
         void OnGUI()
         {
             Draw();
+            PollEvents();
         }
     }
 }
