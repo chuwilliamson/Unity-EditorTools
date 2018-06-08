12c5168727bbbd62d0f9a261e2931a2f5cbfd99d
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-06-08 10:36:02 -0500

    Dynamic Scriptable Menu

--- Assets/Editor/DylanTools/ScriptableVisual.cs
+++ Assets/Editor/DylanTools/ScriptableVisual.cs
@@ -38,26 +38,28 @@ namespace DylanTools
         public ScriptableVisual(Node parent)
         {
             ParentNode = parent;
-            ContentRect = new Rect(ParentNode.Rect);
+            ContentRect = new Rect(ParentNode.Rect);            
             Padding = new Vector2(0, 30);
             ParentNode.scriptableChangedEvent += Draw;
         }
    
         public void Draw(ScriptableObject data)
         {
+            ContentRect = ParentNode.Rect;
             if(data == null)
                 return;
-            GUILayout.BeginArea(ContentRect);
+            GUILayout.BeginArea(new Rect(ContentRect.position - new Vector2(0, -50), ContentRect.size));
             var so = new SerializedObject(data);
-            var properties = data.GetType().GetFields();
+            var properties = data.GetType().GetFields();            
             foreach (var prop in properties)
             {
                 if(prop.IsNotSerialized)
                     continue;
                 var sp = so.FindProperty(prop.Name);
-                EditorGUILayout.PropertyField(sp);
-            }            
-            GUILayout.EndArea();                 
+                EditorGUILayout.PropertyField(sp, new GUIContent(prop.Name), true);                    
+                so.ApplyModifiedProperties();
+            }                  
+            GUILayout.EndArea();            
         }
     }
 }
