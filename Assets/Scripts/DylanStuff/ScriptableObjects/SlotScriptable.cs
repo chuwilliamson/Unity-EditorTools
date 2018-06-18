2db401af6d0792ea6ea9299f2d9788ce1fbc4c77
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-06-18 12:30:11 -0500

    Slots class

---
+++ Assets/Scripts/DylanStuff/ScriptableObjects/SlotScriptable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ScriptableObjects
+{
+    public class SlotScriptable
+    {
+        public bool IsEmpty { get; private set; }
+        public ItemScriptable Item { get; private set; }
+
+        public void AddItem(ItemScriptable item)
+        {
+            if (IsEmpty)
+                Item = item;
+        }
+    }
+}
