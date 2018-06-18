9e7cb3d7968887bc16677e1e7d46dfbd73d19739
Matthew Williamson <chuwilliamson@gmail.com>
2018-06-18 17:17:16 -0500

    setup the animator to receive values through the scriptableobject

---
+++ Assets/Scripts/Assessments/CrossPlatformDevelopment/CharacterAnimationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assessments.CrossPlatformDevelopment
+{
+    public class CharacterAnimationBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Animator _anim;
+
+        [SerializeField] private PlayerData HealthData;
+
+        [SerializeField] private PlayerData HorizontalSpeed;
+
+        [SerializeField] private PlayerData VerticalSpeed;
+
+        private void Start()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _anim.SetFloat(HealthData.Id, HealthData.Value);
+            _anim.SetFloat(HorizontalSpeed.Id, HorizontalSpeed.Value);
+            _anim.SetFloat(VerticalSpeed.Id, VerticalSpeed.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/Assessments/CrossPlatformDevelopment/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assessments.CrossPlatformDevelopment
+{
+    [CreateAssetMenu]
+    public class PlayerData : ScriptableObject
+    {
+        public float Value;
+        private string _name;
+        public int Id;
+        
+        protected virtual void OnEnable()
+        {
+            _name = name;
+            Id = Animator.StringToHash(_name);
+        }
+    }
+}
