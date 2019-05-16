8937668c0f36e1b278f8401dfe00eb851f4ec59b
Matthew Williamson <chuwilliamson@gmail.com>
2019-05-16 12:19:18 -0500

    i have no idea

---
+++ Assets/Scripts/AIE/PhysicsTrigger.cs
@@ -0,0 +1,40 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PhysicsTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameEventArgs onEnter;
+    [SerializeField]
+    GameEventArgs onExit;
+    [SerializeField]
+    GameEventArgs onStay;
+
+    public StringVariable ListenerTag;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(ListenerTag.Value))
+        {
+            onEnter.Raise(gameObject, other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(ListenerTag.Value))
+        {
+            onExit.Raise(gameObject, other.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(ListenerTag.Value))
+        {
+            onStay.Raise(gameObject, other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/AIE/PhysicsTriggerListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PhysicsTriggerListener : MonoBehaviour
+{
+    [HideInInspector] public GameEventArgs onenter;
+    [HideInInspector] public GameEventArgs onstay;
+    [HideInInspector] public GameEventArgs onexit;
+}
+
+[CustomEditor(typeof(PhysicsTriggerListener))]
+public class PhysicsTriggerListenerEditor : Editor
+{
+    private enum PhysicsEvents
+    {
+        OnEnter,
+        OnExit,
+        OnStay
+    }
+
+    private PhysicsEvents selected;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        selected = (PhysicsEvents)EditorGUILayout.EnumPopup(label:"Add New Event", selected:selected);
+        if (EditorGUILayout.DropdownButton(new GUIContent("add new event type"),  FocusType.Passive))
+        {
+            var gm = new GenericMenu();
+            gm.AddItem(new GUIContent("OnEnter"),true,
+                () => { gm.AddDisabledItem(new GUIContent("OnEnter")); });
+            gm.ShowAsContext();
+        }
+        
+
+
+    }
+}
